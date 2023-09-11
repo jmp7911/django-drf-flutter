357f2b05a2a211723852e12e1625fafcf0fbafd1
JiMan Park <jmp7911@gmail.com>
2023-09-11 18:02:39 +0900

    [ADD] Model : record, recordDetail

--- repo/conf/urls.py
+++ repo/conf/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from record.views import ProductListAPI
+from record.views import ProductListAPI, RecordListAPI
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -39,6 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('api/product/list', ProductListAPI.as_view()),
+  path('api/record/<str:user_id>', RecordListAPI.as_view()),
   
   path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
   path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
