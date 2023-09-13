47072a2c05476ee6a2ec877e9069b33c33e83562
doweb <jmp7911@gmail.com>
2023-09-13 15:41:25 +0900

    안쓰는 것 정리

--- repo/record/serializers.py
+++ repo/record/serializers.py
@@ -1,7 +1,3 @@
 from rest_framework import serializers
-from .models import Product
 
-class ProductSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Product        # product 모델 사용
-        fields = '__all__'            # 모든 필드 포함
+
--- repo/record/views.py
+++ repo/record/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from .models import Product
-from rest_framework.views import APIView
-from .serializers import ProductSerializer
 
-class ProductListAPI(APIView):
-  def get(self, request):
-    queryset = Product.objects.all()
-    
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+from rest_framework.views import APIView
+
+
