3bbed27ca556568449279b4aca85d1c3374cd925
doweb <jmp7911@gmail.com>
2023-09-13 15:35:16 +0900

    [ADD] Swagger API Document generate

--- repo/record/serializers.py
+++ repo/record/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Product, Record, RecordDetail
+from drf_yasg import openapi
+
+user_id = openapi.Parameter('user_id', openapi.IN_PATH, description='user_id path', required=True, type=openapi.TYPE_STRING)
 
 class ProductSerializer(serializers.ModelSerializer) :
   class Meta :
@@ -7,6 +10,17 @@ class ProductSerializer(serializers.ModelSerializer) :
     fields = '__all__'
 
 class RecordSerializer(serializers.ModelSerializer) :
+  id = serializers.IntegerField(help_text='Record 객체의 id')
+  class Meta:
+    model = Record
+    fields = ['id']
+class RecordDetailSerializer(serializers.ModelSerializer) :
+  record_id = serializers.IntegerField(help_text='Record 객체의 id')
+  latitude = serializers.FloatField(help_text='위도')
+  longitude = serializers.FloatField(help_text='경도')
+  altitude = serializers.FloatField(help_text='고도')
+  speed = serializers.FloatField(help_text='속도')
   class Meta :
-    model = Record        # product 모델 사용
+    model = RecordDetail
     fields = '__all__'
+
--- repo/record/views.py
+++ repo/record/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.response import Response
 from .models import Product, Record, RecordDetail
 from rest_framework.views import APIView
-from .serializers import ProductSerializer, RecordSerializer
+from .serializers import ProductSerializer, RecordSerializer, RecordDetailSerializer, user_id
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-class ProductListAPI(APIView):
-  def get(self, request):
-    queryset = Product.objects.all()
-    
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
-
+from rest_framework.decorators import action
+from django.utils.decorators import method_decorator
 class RecordListAPI(APIView):
-  user_id = openapi.Parameter('user_id', openapi.IN_PATH, description='user_id path', required=True, type=openapi.TYPE_STRING)
-  
-  @swagger_auto_schema(tags=['지정한 데이터의 상세 정보를 불러옵니다.'], manual_parameters=[user_id], responses={200: 'Success'})
+  @swagger_auto_schema(
+    tags=['지정한 데이터의 상세 정보를 불러옵니다.'],manual_parameters=[user_id],responses={200: 'Success'}
+  )
   def get(self, request, user_id):
     queryset = Record.objects.all()
     
     serializer = RecordSerializer(queryset, many=True)
     return Response(serializer.data)
+class RecordUpdateAPI(APIView):
+  @swagger_auto_schema(
+    tags=['지정한 데이터를 수정합니다.'],request_body=RecordSerializer,responses={200: 'Success'}
+  )
+  def put(self, request, user_id): 
+    pass
+class RecordDetailUpdateAPI(APIView):
+  @swagger_auto_schema(
+    tags=['지정한 데이터를 입력합니다.'],request_body=RecordDetailSerializer,responses={200: 'Success'}
+  )
+  def post(self, request, user_id): 
+    pass
