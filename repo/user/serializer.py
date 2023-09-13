902a40773a76380a09f7efddc8864cc02a743796
doweb <jmp7911@gmail.com>
2023-09-13 16:06:53 +0900

    [ADD] User Django Rest API Document

--- repo/user/serializer.py
+++ repo/user/serializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
 from .models import User
 
-class UpdateUserSerializer(ModelSerializer.serializer):
+class UserSerializer(serializers.ModelSerializer):
+  user_id = serializers.CharField(max_length=20, help_text="사용자 아이디", required=True)
+  name = serializers.CharField(max_length=100, help_text="사용자 이름", required=True)
+  password = serializers.CharField(max_length=20, help_text="사용자 비밀번호", required=True)
+  email = serializers.EmailField(help_text="사용자 이메일", required=True)
+  last_join_at = serializers.DateTimeField()
+  
   class Meta:
     model= User
     fields= '__all__'
--- repo/user/views.py
+++ repo/user/views.py
@@ -3,13 +3,13 @@ from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework.views import APIView
-from .serializer import UpdateUserSerializer
+from .serializer import UserSerializer
 
 # Create your views here.
 class UpdateUserAPI(APIView):
-  @swagger_auto_schema(tags=['사용자의 데이터를 입력합니다.'], request_body=UpdateUserSerializer, responses={200: '성공'})
+  @swagger_auto_schema(tags=['사용자의 데이터를 입력합니다.'], request_body=UserSerializer, responses={200: '성공'})
   def put(self, request):
-    serializer = UpdateUserSerializer(data=request.data)
+    serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=200)
