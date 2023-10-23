e91ddfd2fb9677e4747144f41955d5e28adab509
JiMan Park <jmp7911@gmail.com>
2023-10-23 20:17:00 +0900

    [Complete] login (JWT)

--- repo/accounts/views.py
+++ repo/accounts/views.py
@@ -8,58 +8,6 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_api_key.models import APIKey
 from rest_framework.decorators import api_view, permission_classes
 from datetime import datetime, timedelta
 from rest_framework import status
-from rest_framework_api_key.permissions import HasAPIKey
-
-@api_view(['POST'])
-@csrf_exempt
-def login(request):
-  if request.method == "POST":
-    
-    data = json.loads(request.body)
-    
-    username = data['email']
-    password = data['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        return HttpResponse(user)
-    else:
-        return HttpResponse('로그인 실패. 다시 시도 해보세요.')
-  else:
-    pass
-  return HttpResponse('b')
-
-@api_view(['GET', 'POST'])  
-@csrf_exempt
-@permission_classes([IsAuthenticated])
-def apikey(request):
-    username = request.user.username
-    if request.method == 'GET':
-        response = {}
-        keys = APIKey.objects.all()
-        for key in keys:
-            if key.name == username:
-                response['apikey'] = key.prefix + '*****'
-                response['created'] = key.created
-                response['expires'] = key.expiry_date
-                break
-        return Response(response, status=status.HTTP_200_OK)
-    else:
-        keys = APIKey.objects.all()
-        for key in keys:
-            if key.name == username:
-                key.delete()
-                break
-        api_key, key = APIKey.objects.create_key(name=request.user.username)
-        expires = datetime.now() + timedelta(days=90)
-        api_key.expiry_date = expires
-        api_key.save()
-        return Response({'apikey': key, 'expires': expires}, status=status.HTTP_200_OK)
-      
-@api_view(['GET'])      
-@csrf_exempt      
-def health(request):
-    return Response({'message': 'OK'}, status=status.HTTP_200_OK)
--- repo/conf/urls.py
+++ repo/conf/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from record.views import RecordListAPI, RecordUpdateAPI, RecordDetailUpdateAPI, RecordDetailAPI
-
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,5 +40,6 @@ urlpatterns = [
   # 일반 회원 회원가입/로그인
   path('accounts/', include('allauth.urls')),
   path('api/user/', include('dj_rest_auth.urls')),
-  path('api/user/registration/', include('dj_rest_auth.registration.urls'))
+  path('api/user/registration/', include('dj_rest_auth.registration.urls')),
+  
 ]
