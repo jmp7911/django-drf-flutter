7e7db989c82889d8e32c46c00fb101ff6c4164b3
JiMan Park <jmp7911@gmail.com>
2023-10-25 10:51:18 +0900

    merge

--- flutter_running_mate/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_running_mate/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,4 +8,5 @@ import Foundation
 import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
